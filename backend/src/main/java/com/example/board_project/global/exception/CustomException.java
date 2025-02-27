115fbe2165aeb45d130bd7735eba274747d8bef8
이지연 <jiyeon_0342@naver.com>
2025-02-27 16:38:55 +0900

    Feat: CustomException

---
+++ backend/src/main/java/com/example/board_project/global/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.example.board_project.global.exception;
+
+import com.example.board_project.global.response.ResponseCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final ResponseCode responseCode;
+
+    public CustomException(ResponseCode responseCode) {
+        super(responseCode.getMessage());
+        this.responseCode = responseCode;
+    }
+}
