e5bfb0b24be925c1b21892da0bf64d71fb17f5b1
이지연 <jiyeon_0342@naver.com>
2025-02-27 16:29:01 +0900

    Feat: redis refresh token 관련 config

---
+++ backend/src/main/java/com/example/board_project/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.example.board_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+}
