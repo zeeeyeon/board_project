681d0b2ab97736b168f3f415d8c8e79e8caeff27
이지연 <jiyeon_0342@naver.com>
2025-02-27 17:15:31 +0900

    Refactor: redis 환경 변수 설정

--- backend/src/main/java/com/example/board_project/config/RedisConfig.java
+++ backend/src/main/java/com/example/board_project/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.board_project.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,9 +10,16 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
